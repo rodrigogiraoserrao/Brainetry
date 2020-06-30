b877b0aaaa20a91e25afef5dbd27c12300c35114
Rodrigo Girão Serrão <rodrigogiraoserrao@gmail.com>
2020-07-01 00:52:45 +0100

    Improve user input mechanism.

--- interpreter.py
+++ interpreter.py
@@ -3,7 +3,7 @@ lpp=lambda l:"".join(map(str, l)); ext=lambda m,p:([0]*(p<0)+m+[0]*(p==len(m)),m
 def mpp(m,p=0): return ("(..., "*((li:=max(0,p-7))>0) + "["*(li==0) +
  str(m[li:(ri:=p+8)])[1:-1] + "]"*(ri>=len(m)) + ", ...)"*(ri<len(m)))
 
-def I(l,i,p=0,m=[0],o="",r=0,de=0,_=None):
+def I(l,i=None,p=0,m=[0],o="",r=0,de=0,_=None):
  _W,_eof,_NI,_NO=_
  while l:
   if de>r: print(f"{r}: m[{p}]={m[p]} @ {mpp(m,p)}; inp='{lpp(i)}'; out='{lpp(o)}'\n{lpp(l)}")or input()
@@ -13,6 +13,7 @@ def I(l,i,p=0,m=[0],o="",r=0,de=0,_=None):
    while (n in[2,3]or m[p]) and f: p+=1-[2,18,0,0,3,19].index(n)//2; m,p=ext(m,p); f=n>15
   elif n in[4,5]: m[p]+=1-[4,0,5].index(n); m[p]=_W(m[p])
   elif n in[6,22]:
+   i=sys.stdin.read()if i is None else i; _eof==[0]and (i:=i.split("\n")[:-1])
    while f or(m[p]and n>6): f or (p:=p+1); m,p=ext(m,p); c,*i=i or _eof; m[p]=_W(_NI(c)); f=False
   elif n in[7,23]:
    while (f and n==7)or(m[p]and n>7): print(c:=_NO(m[p]),end="",flush=True); o+=c; n==7 or(p:=p+1); m,p=ext(m,p); f=False
@@ -28,6 +29,4 @@ def E(c,de=0,env=None):
  _W=lambda v: (_w and v%_w) or v; _eof=[0]if _nio else"\u0000"; _NI=int if _nio else ord; _NO=(lambda c:f"{c}\n")if _nio else chr
  _=[_W,_eof,_NI,_NO]
  l=[O[16*(len(l)>1 and l[-1]==l[-2])+len([*filter(bool,l.split(" "))])]for l in c.split("\n")]
- #i=input(" inp >> ")if {*",;"}&{*l} else"0"*_nio; _nio and (i:=i.split("\n"))
- i=print(" inp >> ",end="",flush=True) and sys.stdin.read()if {*",;"}&{*l} else"0"*_nio; _nio and (i:=i.split("\n")[:-1])
- i,p,m,o=I(l,i,r=0,de=de,_=_); print(); return i,p,m,o
+ i,p,m,o=I(l,r=0,de=de,_=_); print(); return i,p,m,o
