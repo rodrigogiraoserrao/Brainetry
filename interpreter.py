dfc932e2a6d40743559e77d6bde4a80ba0989b7e
Bangyen Pham <66754151+bangyen@users.noreply.github.com>
2020-07-01 09:18:24 -0400

    Change number of non-wrapping operators
    - Changes from `12` and `13` to `20` and `21`
    - Allows infinite incrementing for `--w 0`

--- interpreter.py
+++ interpreter.py
@@ -1,4 +1,4 @@
-import sys; O="«»><+-,.[]()±∓    ≥≤  ;:"; Oi=O.index
+import sys; O="«»><+-,.[]()      ≥≤±∓;:"; Oi=O.index
 lpp=lambda l:"".join(map(str, l)); ext=lambda m,p:([0]*(p<0)+m+[0]*(p==len(m)),max(0,p))
 def mpp(m,p=0): return ("(..., "*((li:=max(0,p-7))>0) + "["*(li==0) +
  str(m[li:(ri:=p+8)])[1:-1] + "]"*(ri>=len(m)) + ", ...)"*(ri<len(m)))
@@ -22,7 +22,7 @@ def I(l,i=None,p=0,m=[0],o="",r=0,de=0,_=None):
    g=[t:=t+(Oi(k)==8+d)-(Oi(k)==9+d)for k in l].index(0)
    while m[p] and f:i,p,m,o=I(l[:g+1],i,p,m,o,r+1,de,_); f=n==8
    l=l[g:]
-  elif n in[12,13]: m[p]=sorted([0,m[p]+1-[12,0,13].index(n),_W(-1)])[1]
+  elif n in[20,21]: m[p]=sorted([0,m[p]+1-[20,0,21].index(n)]+[_W(-1)]*bool(_W(-1)+1))[1]
  return i,p,m,o
 
 def E(c,de=0,env=None):
