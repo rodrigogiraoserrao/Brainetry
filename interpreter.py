ae912307398c6716f9a0968fae30efd020c4e6fe
Rodrigo Girão Serrão <rodrigogiraoserrao@gmail.com>
2020-07-03 11:37:47 +0100

    Tweak #6.

--- interpreter.py
+++ interpreter.py
@@ -6,7 +6,7 @@ def mpp(m,p=0): return ("(..., "*((li:=max(0,p-7))>0) + "["*(li==0) +
 def I(l,i=None,p=0,m=None,o="",r=0,de=0,_=None):
  _W,_eof,_NI,_NO=_; m=m or [0]
  while l:
-  if de>r: print(f"{r}: m[{p}]={m[p]} @ {mpp(m,p)}; inp='{lpp(i)}'; out='{lpp(o)}'\n{lpp(l)}")or input()
+  if de>r: print(f"{r}: m[{p}]={m[p]} @ {mpp(m,p)}; inp='{lpp(i)}'; out='{lpp(o)}'\n{lpp(l)}"); input()
   n,*l=l; n=Oi(n); f=True
   if n in[0,1]: p=n*(len(m)-1)
   elif n in[2,3,18,19]:
@@ -22,7 +22,7 @@ def I(l,i=None,p=0,m=None,o="",r=0,de=0,_=None):
    g=[t:=t+(Oi(k)==8+d)-(Oi(k)==9+d)for k in l].index(0)
    while m[p] and f:i,p,m,o=I(l[:g+1],i,p,m,o,r+1,de,_); f=n==8
    l=l[g:]
-  elif n in[20,21]: m[p]=sorted([0,m[p]+1-[20,0,21].index(n)]+[_W(-1)]*bool(_W(-1)+1))[1]
+  elif n in[20,21]: m[p]=sorted([0,m[p]+1-[20,0,21].index(n)]+[_W(-1)]*(_W(-1)!=-1)))[1]
  return i,p,m,o
 
 def E(c,de=0,env=None):
